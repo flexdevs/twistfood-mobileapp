539cd52e29f38431612301a417e73df48c1ae5aa
YorqinBek7 <yorqinbekyuldashev33@gmail.com>
2023-01-12 01:54:35 +0500

    added splash

---
+++ twist_food/lib/utils/styles.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class TwistStyles {
+  static const TextStyle w500 = TextStyle(
+    fontSize: 14.0,
+    fontFamily: 'Viga',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle w400 = TextStyle(
+    fontSize: 13.0,
+    fontFamily: 'Viga',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle w600 = TextStyle(
+    fontSize: 16.0,
+    fontFamily: 'Viga',
+    fontWeight: FontWeight.w600,
+  );
+}
--- twist_food/lib/views/splash/splash_view.dart
+++ twist_food/lib/views/splash/splash_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:twist_food/utils/colors.dart';
+import 'package:twist_food/utils/icons.dart';
+import 'package:twist_food/utils/styles.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,6 +13,34 @@ class SplashView extends StatefulWidget {
 class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              TwistIcons.logo,
+              height: 139.0,
+              width: 175.0,
+            ),
+            Text(
+              'TwistFood',
+              style: TwistStyles.w600.copyWith(
+                fontSize: 40.0,
+                color: TwistColor.primaryColor,
+              ),
+            ),
+            const Text(
+              'Deliever Favorite Food',
+              style: TwistStyles.w500,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
