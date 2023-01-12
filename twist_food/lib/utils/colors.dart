68a64cc54974f671d4f9988f00b4fe8ce16ada28
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2023-01-12 14:36:26 +0500

    o'chib ketgan narsalrni qayta tikladim

--- twist_food/lib/utils/colors.dart
+++ twist_food/lib/utils/colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/animation.dart';
 
 class TwistColor {
   static Color primaryColor = const Color(0xff53E88B);
+
+  static Color C_DA6317 = const Color(0xffDA6317);
+  static Color C_F9A84D = const Color(0xffF9A84D);
+  static Color C_FEAD1D = const Color(0xffFEAD1D);
 }
